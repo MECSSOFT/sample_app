10c3e3597b921e0885030f4ffc6d14344cbfc026
MECS <mecsinformationsystem@gmail.com>
2013-04-19 16:30:21 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'db5d9b291ead1d78e47c62d90d52c8468da3b2c0545570a98e241614eddc28933d22e546ca0023d556a1c8bb4f53b4b57ce83908ce917fbb26f13ee48353a943'
